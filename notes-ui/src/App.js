ca5c0050f55f04895d32f530224d061fba414c25
adarshagc <adarshagc2004@gmail.com>
2026-04-01 13:25:43 +0530

    Fetched and displayed notes from backend

--- notes-ui/src/App.js
+++ notes-ui/src/App.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h1>Notes App</h1>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/notes")
+      .then(res => res.json())
+      .then(data => setNotes(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Notes App</h1>
+
+      {notes.map(note => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
